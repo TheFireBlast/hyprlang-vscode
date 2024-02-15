fc9270fc145f69825e6ae8226062acc0fc899680
TheFireBlast <fireblast901@gmail.com>
2024-02-15 13:29:29 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# VSCode Hyprlang
+
+Adds syntax highlighting for Hyprland config files.
+
+## Syntax Highlighting
+
+<img src="highlighting.png" alt="syntax highlighting" />
---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+
+//TODO: formatting
+//TODO: linting
+//TODO: completions
+//TODO: snippets
+
+export function activate(context: vscode.ExtensionContext) {}
+
+export function deactivate() {}
